f5059f7f0f3029b87b0370c364fe04c31202232a
quatmo <leicanguyen@gmail.com>
2021-12-14 05:58:22 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "Download"
+mkdir /tmp/gmedia
+
+TMPURL="https://raw.githubusercontent.com/quatmo/gmrender-volumio/main"
+TMPPATH="/tmp/gmedia"
+sudo wget -O /$TMPPATH/gmediarender.tar.gz $TMPURL/gmediarender.tar.g
+sudo wget -O /$TMPPATH/gmrender.service.tar.gz $TMPURL/gmrender.service.tar.gz
+
+sudo tar -xzvf /tmp/gmediarender.tar.gz -k -C /
+sudo tar -xzvf /tmp/gmrender.service.tar.gz -k -C /
+
+sudo systemctl daemon-reload
+sudo systemctl enable gmrender.service
+sync
+sudo systemctl restart gmrender.service
+sudo systemctl status gmrender.service
+
+#sudo systemctl stop gmrender.service
