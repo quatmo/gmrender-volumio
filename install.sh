4d3419180892f5ab14d853b1a622a5f78d3b44dc
quatmo <leicanguyen@gmail.com>
2021-12-14 06:26:44 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ echo "Download"
 mkdir /tmp/gmedia
 
 TMPURL="https://raw.githubusercontent.com/quatmo/gmrender-volumio/main"
-TMPPATH="tmp/gmedia"
-sudo wget -O /$TMPPATH/gmediarender.tar.gz $TMPURL/gmediarender.tar.g
-sudo wget -O /$TMPPATH/gmrender.service.tar.gz $TMPURL/gmrender.service.tar.gz
+TMPPATH="/tmp/gmedia"
+sudo wget -O /$TMPPATH/gmediarender.tar.gz https://raw.githubusercontent.com/quatmo/gmrender-volumio/main/gmediarender.tar.gz
+sudo wget -O /$TMPPATH/gmrender.service.tar.gz https://raw.githubusercontent.com/quatmo/gmrender-volumio/main/gmrender.service.tar.gz
 
 sudo tar -xzvf /tmp/gmediarender.tar.gz -k -C /
 sudo tar -xzvf /tmp/gmrender.service.tar.gz -k -C /
